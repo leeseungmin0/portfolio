3e757a38f0103a325b04c3180f6e8b0aa889e516
leeseungmin0 <tmdalsdl112@naver.com>
2022-02-17 01:01:27 +0900

    JS-home

--- main.js
+++ main.js
@@ -25,8 +25,22 @@ homeContactBtn.addEventListener('click',() => {
     scrollIntoView('#contact');
 });
 
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
 
